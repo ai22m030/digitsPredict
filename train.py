15db819165a95a107f708c4fe51d0f3fb6f7d62c
adva <adnan.vatric@jumptoday.group>
2022-09-15 20:52:43 +0200

    initial commit

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 from sklearn import datasets, svm
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import pickle
+from joblib import dump
 
 digits = datasets.load_digits()
 
@@ -13,16 +12,4 @@ clf = svm.SVC()
 
 clf.fit(X_train, y_train)
 
-
-'''
-predicted = clf.predict(X_test)
-
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Prediction: %i" % prediction)
-
-plt.show()
-'''
+dump(clf, 'digits.joblib')
