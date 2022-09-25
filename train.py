a7ba0468e02f9aabb54956c1eca2f81f11974306
adva <adnan.vatric@jumptoday.group>
2022-09-25 13:45:09 +0200

    refactor and add comments

--- train.py
+++ train.py
@@ -3,17 +3,21 @@ import sys
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 
 if __name__ == '__main__':
+    # Init some values
+    img_rows = img_cols = 28
+    mnist_dataset = tf.keras.datasets.mnist
+
+    # Parse argument if is passed
     if len(sys.argv) > 1:
         filename = sys.argv[1] + '.h5'
     else:
         filename = 'assignment01_model.h5'
 
-    mnist_dataset = tf.keras.datasets.mnist
-
-    img_rows = img_cols = 28
+    # Load data and turn values into float
     (x_train, y_train), (x_test, y_test) = mnist_dataset.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
+    # Build up model
     model = tf.keras.models.Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
@@ -30,6 +34,8 @@ if __name__ == '__main__':
                   loss="sparse_categorical_crossentropy",
                   metrics=["accuracy"])
 
+    # Train model
     model.fit(x_train, y_train, batch_size=128, epochs=5, verbose=1, validation_data=(x_test, y_test))
     model.save(filename)
+
     print(f"The model was saved as '{filename}' and can be used with consume.py -m {filename}")
