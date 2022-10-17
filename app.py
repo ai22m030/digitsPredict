c89d983280bbcd977832211999158b17c6acc6a7
ai22m030 <ai22m030@technikum-wien.at>
2022-10-17 06:05:58 +0200

    add rest api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 from consume_lib import predict
 from flask import Flask, request, jsonify, abort
 from werkzeug.utils import secure_filename
@@ -6,6 +7,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_PATH'] = 'uploads'
+app.config['PREDICT_PATH'] = 'predictions'
 
 
 @app.route('/', methods=['POST'])
@@ -14,7 +16,25 @@ def upload_files():
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
         file = os.path.join(app.config['UPLOAD_PATH'], filename)
+        predict_file_name = uuid.uuid4().__str__()
+        predict_file_path = os.path.join(app.config['PREDICT_PATH'], predict_file_name)
         uploaded_file.save(file)
-        return jsonify(prediction=predict(filename, "assignment01_model.h5", False).__str__())
+
+        predict_file = open(predict_file_path, 'w')
+        predict_file.write(predict(filename, "assignment01_model.h5", False).__str__())
+
+        return jsonify(file_name=predict_file_name)
+
+    abort(500)
+
+
+@app.route('/', methods=['GET'])
+def upload_files():
+    predict_file_name = request.args.get('file_name')
+    filename = secure_filename(predict_file_name)
+    if filename != '':
+        file = os.path.join(app.config['PREDICT_PATH'], filename)
+        predict_file = open(file, 'r')
+        return jsonify(prediction=predict_file.readline())
 
     abort(500)
